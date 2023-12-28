d4cfd74c69b6900249f10a2bc580fdcca65a8975
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 17:18:09 +0500

    feat: graceful shutdown

--- main.go
+++ main.go
@@ -3,14 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	"github.com/theoreotm/go-data-api/application"
 )
 
 func main() {
-	app := application.New()
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
 
-	err := app.Start(context.TODO())
+	// Get environment variables
+	redisPortStr := os.Getenv("REDIS_PORT")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	redisHost := os.Getenv("REDIS_HOST")
+
+	// Convert the port to an integer
+	redisPort, err := strconv.Atoi(redisPortStr)
+	if err != nil {
+		fmt.Println("Error converting REDIS_PORT to integer:", err)
+		return
+	}
+
+	app := application.New(redisPassword, redisHost, redisPort, 0)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err = app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
